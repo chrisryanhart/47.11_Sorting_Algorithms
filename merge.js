fa6c9d2de78bc8b40a40c0c959180c0fb358d748
rrghiker <chris.ryan.hart1@gmail.com>
2023-01-12 10:53:07 -0500

    finished exercise

--- merge.js
+++ merge.js
@@ -1,5 +1,48 @@
-function merge() {}
+function merge(arr1, arr2) {
+    let sortedArr = [];
+    let i = 0;
+    let j = 0;
 
-function mergeSort() {}
+    // while length less than arr1
+    // while length less than arr2
+    while(i < arr1.length && j < arr2.length){
+        if(arr1[i] < arr2[j]){
+            sortedArr.push(arr1[i]);
+            i++;
+        }else{
+            sortedArr.push(arr2[j]);
+            j++
+        }
+    }
+
+    while(i < arr1.length){
+        sortedArr.push(arr1[i]);
+        i++;
+    }
+
+    while(j < arr2.length){
+        sortedArr.push(arr2[j]);
+        j++;
+    }
+
+    return sortedArr;
+}
+
+function mergeSort(arr) {
+
+    if(arr.length <= 1) return arr;
+
+    const midpoint = Math.floor(arr.length/2);
+
+    const firstArrHalf = arr.slice(0,midpoint);
+    const secondArrHalf = arr.slice(midpoint);
+
+    const sortedFirstHalf = mergeSort(firstArrHalf);
+    const sortedSecondHalf = mergeSort(secondArrHalf);
+
+    const sortedArr = merge(sortedFirstHalf,sortedSecondHalf);
+
+    return sortedArr;
+}
 
 module.exports = { merge, mergeSort};
